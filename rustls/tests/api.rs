f2d9e9385c445d921466126ac3d0b30c0d921c5d
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-06-22 17:11:58 +0200

    quic: re-expose secrets with a high-level public API
    Rolls back some of the changes from #362 while satisfying the
    concerns from #711 and #748.

--- rustls/tests/api.rs
+++ rustls/tests/api.rs
@@ -2960,7 +2960,7 @@ mod test_quic {
         recv: &mut dyn Connection,
     ) -> Result<Option<quic::Keys>, Error> {
         let mut buf = Vec::new();
-        let secrets = loop {
+        let change = loop {
             let prev = buf.len();
             if let Some(x) = send.write_hs(&mut buf) {
                 break Some(x);
@@ -2974,7 +2974,11 @@ mod test_quic {
         } else {
             assert_eq!(recv.alert(), None);
         }
-        Ok(secrets)
+
+        Ok(change.map(|change| match change {
+            quic::KeyChange::Handshake { keys } => keys,
+            quic::KeyChange::OneRtt { keys, .. } => keys,
+        }))
     }
 
     #[test]
